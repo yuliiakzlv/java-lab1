86bfd56ee1e7870804379834aa16b4852d3cf32a
YuliiaKozlova <131869548+yuliiakzlv@users.noreply.github.com>
2023-10-09 12:13:31 +0200

    Add files via upload

---
+++ java/zad2/src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello there!");
+
+        int x, y;
+        int z = 12;
+        double i = 2.45;
+        char znak = 'z';
+        double j;
+        x = 0;
+        y = 444;
+        j = 0.002;
+        System.out.println("x = "+ x +", y = "+ y +", z = "+ z );
+        System.out.println("i= "+i+", znak = "+znak+", j = "+j );
+        System.out.printf("\n i=%4.1f, j=%8.2e, y=%5d, x=%5d, z=%5d, znak:%c", i, j, y, z,
+                x, znak);
+
+
+    }
+}
